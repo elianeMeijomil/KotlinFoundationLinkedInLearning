82e441ea77e01990ea9211c777f727ad680ff3d4
elianeMeijomil <li.meijomil@gmail.com>
2025-02-26 22:11:49 -0300

    [fix] Fixando problemas não resolvidos durante o curso, deixados como desafio

--- src/Agenda.kt
+++ src/Agenda.kt
@@ -9,15 +9,34 @@ fun main(){
         when (choice) {
             1 -> contacts.add(createContact())
             2 -> listContacts(contacts)
-            3 -> println("Função para remover contatos")
+            3 -> {
+                var deleteNumber : String = deleteContacts(contacts)
+                try {
+                    contacts.removeAt(deleteNumber.toInt()-1 )
+                    println("\n==============================")
+                    println("Registro removido com sucesso!")
+                    println("==============================\n")
+                } catch (e:NumberFormatException){
+                    println("Por favor, digite um número")
+                } catch(e: IndexOutOfBoundsException){
+                    println("\n==============================")
+                    println("Registro não encontrado")
+                    println("==============================\n")
+
+                }
+            }
             else -> println("Opção inválida")
         }
     } while (choice != 0)
-    println("Até logo!")
+    println("\n==============================")
+    println("           Até breve!")
+    println("==============================\n")
 }
 
 fun showMenu(){
-    println("===MENU===")
+    println("\n==============================")
+    println("=========== MENU =============")
+    println("==============================\n")
     println("1. Novo contato")
     println("2. Listar contatos")
     println("3. Remover contato")
@@ -26,7 +45,13 @@ fun showMenu(){
 
 fun getChoice(): Int{
     print("> ")
-    return readln().toInt()
+    var choice = 0
+    try {
+        choice = readln().toInt()
+    } catch (e:NumberFormatException){
+         //Log("Opção inválida")
+    }
+    return choice ?: 0
 }
 
 /* CRUD */
@@ -39,6 +64,7 @@ fun createContact() : Map<String, String> {
     val phonenumber = readln()
 
     println("Contato cadastrado com sucesso")
+    //return mapOf("id" to id, "name" to name, "phonenumber" to phonenumber)
     return mapOf("name" to name, "phonenumber" to phonenumber)
 }
 
@@ -47,15 +73,22 @@ fun listContacts(contacts: List<Map<String,String>>) {
         println("Nenhum contato cadastrado")
     }
     try {
-        for (contact in contacts) {
+        for ((index,contact) in contacts.withIndex()) {
+            print("[${index+1}]: ")
             for (key in contact.keys) {
                 print("$key: "+contact[key]+" |")
             }
         println()
-        //println("[] Nome: ${contact["name"]} | Telefone: ${contact["phonenumber"]}")
+        //println("Nome: ${contact["name"]} | Telefone: ${contact["phonenumber"]}")
         }
     } catch (e:Exception){
         println("Deu ruin!")
     }
 }
-fun deleteContact() {}
+
+fun deleteContacts(contacts: List<Map<String,String>>) : String{
+    listContacts(contacts)
+    println("digite o número do contato que que deseja remover")
+    print(" > ")
+    return (readln())
+}
